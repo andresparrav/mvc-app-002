1ccf9b9de00e9169ebd3ec498f9e406f744dda33
Andrw <andresparrav@outlook.com>
2025-10-23 22:06:00 -0500

    Cambios

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "main" {
     application = var.app_name
   }
 }
-/*
+
 resource "azurerm_service_plan" "main" {
   name                = "${var.app_name}-${var.environment}-asp"
   resource_group_name = azurerm_resource_group.main.name
@@ -43,4 +43,4 @@ resource "azurerm_linux_web_app" "main" {
     application = var.app_name
   }
 }
-*/
+
